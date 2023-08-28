a75695d5896bf23e7eb1b19875100b25396123d7
ana <anabellemahadevan@gmail.com>
2023-08-28 13:45:12 -0600

    Initial Commit

---
+++ Shared/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  Shared
+//
+//  Created by anabelle mahadevan on 8/28/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
