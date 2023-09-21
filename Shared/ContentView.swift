01f072518df763ffe3f086c98e59b8be60bff294
ana <anabellemahadevan@gmail.com>
2023-09-21 16:08:24 -0600

    the code is working right now

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -2,122 +2,67 @@
 //  ContentView.swift
 //  Shared
 //
-//  Ana Mahadevan, Assignment #3 for Mobile App Development
+//  Ana Mahadevan, Assignment #4 for Mobile App Development
 //
+// Read data from an API.
+
+// Display an array of data in a List.
+
+// Display an individual element on a view of its own.
 
 import SwiftUI
-import UIKit
 
-struct ContentView: View {
-    var body: some View {
-        TabView { // Navigation Menu !
+// struct for each fruit
+
+struct Fruit: Codable, Identifiable {
+    var id: Int
+    var name: String
+    var nutritions: Nutrition
+}
+
+struct Nutrition: Codable {
+    var calories: Double
+    var protein: Double
+}
+
+struct FruitView: View {
+    @State var fruits =  [Fruit]()
+    
+    func getFruit() async -> () {
+        do {
+
+//            let calString = String(format: "%.2f", quantityValue)
+//            let calShort = Double(Int(100*Double(calString)!))/100
             
-            IntroPageView()
-                .tabItem{
-                    Label("home", systemImage: "globe")
-                }
-        
-            Assignment2View()
-                .tabItem {
-                    Label("#2", systemImage: "list.dash")
-                }
-
-            OtherPageView()
-                .tabItem {
-                    Label("Order", systemImage: "square.and.pencil")
-                }
+            let url = URL(string: "https://www.fruityvice.com/api/fruit/all")!
+            let (data, _) = try await URLSession.shared.data(from: url)
+            print(data)
+            fruits = try JSONDecoder().decode([Fruit].self, from: data)
+        } catch {
+            print("Error: \(error.localizedDescription)")
         }
     }
-}
-    
-struct IntroPageView: View{
-   // empty array for images
-    var images: [Image] = []
-    // controls width of columns
-    let columns = [
-            GridItem(.flexible()),
-            GridItem(.flexible()),
-            GridItem(.flexible())
-                  ]
     
     var body: some View {
-       NavigationView {
-// for loop to
-       VStack {
-
-        HStack {
-            //whatever number is here first is the center image
-            ForEach (1..<2){ i in
-            Image("Grid_\(i)")
-            .resizable()
-            .aspectRatio(contentMode: .fill)
+        NavigationView {
+            List(fruits) { fruit in
+                VStack(alignment: .leading) {
+                   Text("\(fruit.name)")
+                   Text("Calories : \(fruit.nutritions.calories)")
+                    Text("Protein : \(fruit.nutritions.protein)")
+                   
+                }
             }
-         }
-           
-           HStack {
-           ForEach (1..<4){ i in
-           Image("Grid_\(i)")
-           .resizable()
-           .aspectRatio(contentMode: .fill)                       }
-                  }
-           }
-    .navigationTitle("Home")
-
-
-
-    }
-  }
-}
-    
-struct Assignment2View: View{
-    @State private var beExcited = false
-    var text = "bmo"
-    
-// function to make uppercase and append "!"
-    func makeChange(_ input: String) -> String {
-        //when the toggle is on, beExcited = true + make uppercase
-        return beExcited ? input.uppercased() : input
-    }
-
-    var body: some View {
-        VStack {
-            Image("bmo")
-                .resizable()
-                .frame(width: 320, height: 420)
-                .border(Color.teal, width: 8)
-
-            Toggle("get excited!", isOn: $beExcited)
-
-            Text("\(makeChange(text))")
-                .padding(10)
-                .font(.system(size: 20, weight: .bold))
-            } .padding(10);
+            .task {
+                await getFruit()
+            }
         }
-}
-    
-
-struct OtherPageView: View {
-    //create variable for when alert is on
-    @State private var showAlert = false
-    var body: some View {
-        
-        //when button is clicked show
-        Button("click here...") {
-               showAlert = true
-           }
-
-        .alert(isPresented: $showAlert) {
-                Alert(
-                    title: Text("meep!"),
-                    message: Text("meep meep meep")
-                )
-            }
+        .navigationTitle("Fruits")
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct FruitView2: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        FruitView()
     }
 }
-
