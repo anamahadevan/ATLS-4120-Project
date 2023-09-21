5704efb199e65bea997ab84331260fa91a87033c
ana <anabellemahadevan@gmail.com>
2023-09-21 17:09:10 -0600

    with tab view

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -12,6 +12,24 @@
 
 import SwiftUI
 
+
+struct ContentView: View {
+    var body: some View {
+        TabView { // Navigation Menu !
+            
+            FruitView()
+                .tabItem{
+                    Label("Fruits", systemImage: "apple")
+                }
+            GalleryView()
+                .tabItem{
+                    Label("Gallery", systemImage: "globe")
+                }
+        }
+    }
+}
+    
+
 // struct for each fruit
 
 struct Fruit: Codable, Identifiable {
@@ -44,30 +62,59 @@ struct FruitView: View {
     }
     
     var body: some View {
-        VStack{
-     
+        
             NavigationView {
                 List(fruits) { fruit in
                     VStack(alignment: .leading) {
                        Text("\(fruit.name)")
                        Text("Calories : \(fruit.nutritions.calories, specifier: "%.2f")")
                         Text("Protein: \(fruit.nutritions.protein, specifier: "%.2f")")
-                       
                     }
+                    
                 }
                 .task {
                     await getFruit()
                 }
             }
             // is this not working?
-            .navigationTitle("Fruits")
-        }
+            .navigationTitle("Home")
      
     }
 }
 
-struct prevewfruit: PreviewProvider {
+struct GalleryView: View{
+   // empty array for images
+    var images: [Image] = []
+    // controls width of columns
+    let columns = [ GridItem(.flexible()), GridItem(.flexible())]
+    
+    var body: some View {
+        NavigationView{
+            VStack{
+                HStack{
+                    ForEach(1..<2){ i in
+                        Image("Grid_\(i)")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                    }
+                }
+            }
+            HStack{
+                ForEach(1..<4){ i in
+                    Image("Grid_\(i)")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                }
+            }
+        
+        }
+        .navigationTitle("Gallery")
+    }
+  }
+
+
+struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        FruitView()
+        ContentView()
     }
 }
